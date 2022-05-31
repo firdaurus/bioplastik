5052bb723b4294407c7648e64c2a7d229c683744
Chesa <chesakeselekkebo@gmail.com>
2022-06-01 00:38:16 +0700

    change to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import DetailMateri from './components/DetailMateri';
 import ListSections from './components/ListSections';
