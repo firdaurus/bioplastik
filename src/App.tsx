ea18bb2ee0b322b927da94c89faad914ca94941b
Chesa <chesakeselekkebo@gmail.com>
2022-06-01 11:24:08 +0700

    Delete oldfolder

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import DetailMateri from './components/DetailMateri';
 import ListSections from './components/ListSections';
 import CaptureScan from './pages/CaptureScan';
 import KunciDeterminasi from './pages/KunciDeterminasi';
-import Loading from './pages/Loading';
 import Materi from './pages/Materi';
 import Nilai from './pages/Nilai';
 import Salah from './pages/Salah';
@@ -25,7 +24,6 @@ function App() {
           <Route index element={<ListSections />} />
           <Route path=":sectionName" element={<DetailMateri />} />
         </Route>
-        <Route path="/loading" element={<Loading />} /> {/* TODO: Remove this */}
         <Route path="/nilai" element={<Nilai />} />
         <Route path="/qr/:hash" element={<CaptureScan />} />
         <Route path="/kuis" element={<Kuis />} />
--- temp/utils/DB.ts
+++
@@ -1,9 +0,0 @@
-import Dexie from 'dexie'
-
-const db = new Dexie('bioplastik')
-
-db.version(1).stores({
-    data: 'key, value'
-});
-
-export default db
